c4e32e9b40bf701de28721d6bd8f86ab84555b09
Virajkooper <96483980+Virajkooper@users.noreply.github.com>
2024-09-04 01:26:38 +0530

    User Creation

---
+++ restaurant-app/src/main/java/com/example/restaurant/entity/Reservation.java
@@ -0,0 +1,120 @@
+package com.example.restaurant.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reservations")
+public class Reservation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "phone")
+	private String phone;
+
+	@Column(name = "person")
+	private int person;
+
+	@Column(name = "date")
+	private String date;
+
+	@Column(name = "time")
+	private String time;
+
+	@Column(name = "message")
+	private String message;
+
+	public Reservation() {
+	}
+
+	public Reservation(Long id, String name, String email, String phone, int person, String date, String time,
+			String message) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.person = person;
+		this.date = date;
+		this.time = time;
+		this.message = message;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getPerson() {
+		return person;
+	}
+
+	public void setPerson(int person) {
+		this.person = person;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ restaurant-app/src/main/java/com/example/restaurant/entity/User.java
@@ -0,0 +1,95 @@
+package com.example.restaurant.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "phoneno")
+	private String phoneno;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "role")
+	private String role; // ADMIN, STAFF, CUSTOMER
+
+	public User() {
+	}
+
+	public User(Long id, String username, String email, String phoneno, String password, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.phoneno = phoneno;
+		this.password = password;
+		this.role = role;
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneno() {
+		return phoneno;
+	}
+
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
